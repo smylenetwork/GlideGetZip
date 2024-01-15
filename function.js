abfbb81bcb7ac1a164aba4cceed0e4de96267d8e
smylenetwork <156674787+smylenetwork@users.noreply.github.com>
2024-01-15 16:39:29 +0200

    Add files via upload

---
+++ function.js
@@ -0,0 +1,8 @@
+window.function = async function () {
+  // Use fetch API to get IP address
+  const response = await fetch('https://ipapi.co/json/');
+  const data = await response.json();
+
+  // Return the IP address
+  return data.city;
+}
